9ae4afbccc948908a338c29e608be9b305158858
Aleksandar Beker <aleksandar.beker@argusdatainsights.de>
2024-10-28 11:47:30 +0100

    add a retry policy for an external api

--- RPSSL.Infrastructure/Persistence/RandomNumberRepository.cs
+++ RPSSL.Infrastructure/Persistence/RandomNumberRepository.cs
@@ -1,17 +1,27 @@
 ﻿using CSharpFunctionalExtensions;
+using Microsoft.Extensions.Logging;
+using Polly;
 using RPSSL.Application.Choices.Persistence;
 using RPSSL.Domain.Common.Lists;
 using RPSSL.Infrastructure.ApiClients.CodeChallenge;
 
 namespace RPSSL.Infrastructure.Persistence;
 
-public class RandomNumberRepository(ICodeChallengeApiClient codeChallengeApiClient) : IRandomNumberRepository
+public class RandomNumberRepository(ICodeChallengeApiClient codeChallengeApiClient, ILogger<RandomNumberRepository> logger) : IRandomNumberRepository
 {
     public async Task<Result<int, ErrorList>> GenerateAsync(CancellationToken cancellationToken)
     {
-        // todo: add retry policy and error handling
-        
-        var response = await codeChallengeApiClient.GetRandomNumberAsync();
+        var retryPolicy = Policy
+            .Handle<HttpRequestException>()
+            .WaitAndRetryAsync(
+                retryCount: 3,
+                sleepDurationProvider: attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)),
+                onRetry: (exception, timespan, attempt, _) =>
+                {
+                    logger.LogWarning($"Retry {attempt} encountered an error: {exception.Message}. Waiting {timespan} before next retry.");
+                });
+
+        var response = await retryPolicy.ExecuteAsync(async () => await codeChallengeApiClient.GetRandomNumberAsync());
 
         return response.RandomNumber;
     }
